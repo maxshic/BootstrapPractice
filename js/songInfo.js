47fc0f848d375e6c7a4d4bc52f1aca07186b453c
最爱滕小厨 <shichong@teng520.top>
2017-09-15 22:09:24 +0800

    2017-09-15

--- js/songInfo.js
+++ js/songInfo.js
@@ -3,9 +3,16 @@
  */
 $(function(e){
 
+    $('#form').submit(function(){
+        return false;
+    });
+
     var songCateObjs = [];
+    var songInfoObjs = [];
 
     loadSongCateOpt();
+
+    loadSongInfo();
 
     function loadSongCateOpt(){
         $.ajax({
@@ -13,7 +20,7 @@ $(function(e){
             data: '',
             method: 'get',
             success: function(data){
-                console.log(data);
+                //console.log(data);
                 songCateObjs = data.Data;
                 for(var i = 0; i < songCateObjs.length; i++){
                     createSongCateOpt(songCateObjs[i]).appendTo('#selCate');
@@ -32,4 +39,41 @@ $(function(e){
         $opt.text(item.SongCategory.Name);
         return $opt;
     };
+
+    //
+    function loadSongInfo(){
+        $.ajax({
+            url: 'http://192.168.9.100/htmlprojectwebapi/song/list',
+            data: '',
+            method: 'get',
+            success: function(data){
+                console.log(data);
+                songInfoObjs = data.Data;
+            },
+            dataType: 'json'
+        });
+    };
+
+    //
+    $('#btnSearch').bind('click' ,function(e){
+        var $temp = Array.from(songInfoObjs);
+        if($('#selCate').val() != 0){
+            //console.log($('#selCate').val());
+            //console.log($temp);
+            for(var i = $temp.length - 1; i >= 0 ; i--){
+                if($temp[i].CategoryName !== $('#selCate').val()){
+                    $temp.splice(i ,1);
+                }
+            }
+            //console.log($temp);
+        }
+        if($('#selSingerName').val() != ''){
+            for(var i = $temp.length - 1; i >= 0; i--){
+                if($temp[i].SinerName.indexOf($('#selSingerName').val()) == -1){
+                    $temp.splice(i ,1);
+                }
+            }
+        }
+        console.log($temp);
+    });
 });
