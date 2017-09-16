b604d0871eaded50621118cbb6d68d191b4bd1fc
最爱滕小厨 <shichong@teng520.top>
2017-09-16 22:07:49 +0800

    2017-09-16

--- js/songInfo.js
+++ js/songInfo.js
@@ -3,12 +3,16 @@
  */
 $(function(e){
 
+    $('[data-toggle="popover"]').popover();
+
     $('#form').submit(function(){
         return false;
     });
 
     var songCateObjs = [];
     var songInfoObjs = [];
+    var $copy = $('#tbMain>tr:first').remove();
+    var pageSize = 5;
 
     loadSongCateOpt();
 
@@ -32,7 +36,7 @@ $(function(e){
     };
 
 
-    //
+    //加载歌曲分类下拉框
     function createSongCateOpt(item){
         var $opt = $('<option></option>');
         $opt.attr({value:item.SongCategory.Name});
@@ -40,21 +44,73 @@ $(function(e){
         return $opt;
     };
 
-    //
+    //加载歌曲信息
     function loadSongInfo(){
         $.ajax({
             url: 'http://192.168.9.100/htmlprojectwebapi/song/list',
             data: '',
             method: 'get',
             success: function(data){
-                console.log(data);
+                //console.log(data);
                 songInfoObjs = data.Data;
+                /*for(var i = 0; i < songInfoObjs.length; i++){
+                    createSongInfoItem(songInfoObjs[i] ,i).appendTo('#tbMain');
+                }*/
+                createSongInfoList(songInfoObjs);
             },
             dataType: 'json'
         });
     };
 
-    //
+    //创建歌曲列表项
+    function createSongInfoItem(item ,i){
+        var $tr = $copy.clone();
+        $tr.find(':nth-child(1)').text(i + 1);
+        $tr.find(':nth-child(2)>img').prop('src' ,item.Image);
+        $tr.find(':nth-child(3)').text(item.Name);
+        $tr.find(':nth-child(4)').text(item.SinerName);
+        $tr.find(':nth-child(5)').text(item.CategoryName);
+        $tr.find(':nth-child(6)').text(item.PlayNumber);
+        $tr.find(':nth-child(7)>button').text('歌词').attr({'title':item.Name ,'data-content':item.Word}).popover();
+
+        return $tr;
+    };
+
+    //创建歌曲列表
+    function createSongInfoList(arr){
+        $('#tbMain').html('');
+        $('.pagination').html('');
+        if(arr.length != 0){
+
+            var pageLength = Math.ceil(arr.length / pageSize);
+            for(var i = 0; i < pageLength; i++){
+                var $li = $('<li><a href="#"></a></li>');
+                $li.appendTo('.pagination').find('a').text(i + 1);
+                $li.bind('click' , i ,function(e){
+                    e.preventDefault();
+                    e.stopPropagation();
+                    $('#tbMain').html('');
+                    //$('.pagination').html('');
+                    var startIndex = e.data * pageSize;
+                    var endIndex = (e.data + 1) * pageSize < arr.length ? (e.data + 1) * pageSize : arr.length;
+                    //console.log(startIndex +':'+ endIndex);
+                    for(var i = startIndex; i < endIndex; i++){
+                        createSongInfoItem(arr[i] ,i).appendTo('#tbMain');
+                    }
+                });
+            }
+
+
+            for(var i = 0; i < pageSize && i < arr.length; i++){
+                createSongInfoItem(arr[i] ,i).appendTo('#tbMain');
+            }
+        }else{
+            $('.pagination').text('暂无数据哦');
+        }
+
+    };
+
+    //查询
     $('#btnSearch').bind('click' ,function(e){
         var $temp = Array.from(songInfoObjs);
         if($('#selCate').val() != 0){
@@ -74,6 +130,8 @@ $(function(e){
                 }
             }
         }
-        console.log($temp);
+        //console.log($temp);
+
+        createSongInfoList($temp);
     });
 });
---
+++ js/uploadSongInfo.js
@@ -0,0 +1,101 @@
+/**
+ * Created by Administrator on 2017/9/16.
+ */
+$(function(e){
+
+    //fileReader封面
+    $('#songHeader')[0].onchange = function(e){
+        var reader = new FileReader();
+        reader.onload = function(e){
+            $('#songHeaderImg').prop('src' ,this.result);
+        };
+        reader.readAsDataURL($('#songHeader')[0].files[0]);
+    };
+
+    $('#btnSongSource').click(function(e){
+        $('#songSource').click();
+    });
+
+    //获取MP3文件和时长
+    $('#songSource')[0].onchange = function(e){
+        var self = this;
+
+        if(self.files[0].type == 'audio/mp3' && self.files[0].size <= 2*1024*1024){
+            var reader = new FileReader();
+            $('#txtSongSource').val(self.value);
+            reader.onload = function(e){
+                console.log(e);
+                $('#audio').prop('src' ,this.result);
+
+                $('#audio').on('canplay' ,function(e){
+                    $('#songDuration').val($('#audio')[0].duration.toFixed(0));
+                });
+
+            };
+            reader.readAsDataURL($('#songSource')[0].files[0]);
+        }else{
+            console.log('error');
+        }
+    };
+
+    loadCateList();
+
+    //加载歌曲分类列表
+    function loadCateList(){
+        $.ajax({
+            url: 'http://192.168.9.100/htmlprojectwebapi/SongCategory/list',
+            data: '',
+            method: '',
+            success: function(data){
+                var objs = data.Data;
+                for(var i = 0; i < objs.length; i++){
+                    var $opt = $('<option></option>');
+                    $opt.text(objs[i].SongCategory.Name).attr({'value':objs[i].SongCategory.Id}).appendTo('#songCate');
+                }
+            },
+            dataType: 'json'
+        });
+    };
+
+
+
+    loadSingerList();
+
+    //加载歌手列表项
+    function loadSingerList(){
+        $.ajax({
+            url: 'http://192.168.9.100/htmlprojectwebapi/singer/list',
+            data: '',
+            method: 'get',
+            success: function(data){
+                console.log(data);
+                var objs = data.Data;
+                for(var i = 0; i < objs.length; i++){
+                    var $opt = $('<option></option>');
+                    $opt.text(objs[i].Name).attr({'value':objs[i].Id}).appendTo('#songSinger');
+                }
+            },
+            dataType: 'json'
+        });
+    };
+
+    //上传音乐
+    $('#btnUpload').bind('click' ,function(e){
+        var form = $('#form')[0];
+        var formData = new FormData(form);
+        $.ajax({
+            url: 'http://192.168.9.100/htmlprojectwebapi/song/create',
+            data: formData,
+            method: 'post',
+            contentType:false,
+            processData:false,
+            cache:false,
+            success: function(data){
+                console.log(data);
+                if(data.Code == 100){
+                    location.href = 'songInfo.html';
+                }
+            }
+        });
+    });
+});
